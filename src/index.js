37e722b22074fbcbb15a38f0123b7bc46166761c
chuhx <chuhx@yonyou.com>
2019-11-06 14:44:17 +0800

    用函数式的方式创建了一个 组件

--- src/index.js
+++ src/index.js
@@ -1,7 +1,28 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-
+// const createApp = (props) => {
+//     return (
+//         <div>
+//             {/* 只要是要写js 就加一层花括号  注释也是js 所以也要加 */}
+//             <h1>{props.title}</h1>
+//             <p>{props.name}</p>
+//         </div>
+//     ) 
+// }
+// const app = createApp({
+//     title: '你好',
+//     name: '夏明'
+// })
+// 第一种创建组件的方式  函数式   上边就是原理  
+const App = (props) => {
+    return (
+        <div>
+            <p>{props.name}</p>
+            <p>{props.title}</p>
+        </div>
+    )
+}
 ReactDOM.render(
-    <h1>我的项目起来了</h1>,
+    <App title="你好" name="小明"/>,
     document.querySelector('#root')
 )
