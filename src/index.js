3d72ddd1711b73ced785309fc5030a233908faf8
chuhx <chuhx@yonyou.com>
2019-11-06 16:34:36 +0800

    使用 classnames 第三方包 实现 给div 动态设置 类名

--- src/index.js
+++ src/index.js
@@ -1,15 +1,25 @@
 import React from 'react'
 import { render } from "react-dom"
 
-// 定义组件的第二个方式 使用类
+// 给元素动态设置 类名的第三方包   
+import classNames from 'classnames'
+
+import './index.css'
+
+// 定义组件的第二个方式 使用类 继承
 
 class App extends React.Component {
+  
     render () {
+        const style = {
+            color: 'red'
+        }
         console.log(this.props)
         return (
             <div>
-                <h>我就是类创建出来的组件</h>
-                <div>{this.props.msg}</div>
+                <h style={style}>我就是类创建出来的组件</h>
+                <div className="has-text-red">{this.props.msg}</div>
+                <div className={classNames('a',{b: true}, {c: false})}>{this.props.msg}</div>
             </div>
         )    
     }
